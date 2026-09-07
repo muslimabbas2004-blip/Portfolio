85177cc499fc9aa5977f9e47d0dcdf0bbd50cb62
Muslim Abbas <muslimabbas2004@gmail.com>
2026-09-07 21:30:20 +0500

    feat: add dotenv support and fix package.json BOM

--- server.js
+++ server.js
@@ -5,6 +5,9 @@
  * Listens on process.env.PORT (set automatically by Railway).
  */
 
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
